1d6de8e0d8039830ff6f14856ffc23fb40f01d2b
Yana <60299y@mail.ru>
2023-07-14 17:34:07 +0200

    begin todo

---
+++ src/components/TODOList.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { reactive } from 'vue'
+import { addModal } from '../components/addModal.vue'
+
+const TODOArray = reactive([])
+
+const newTODOInfo = reactive({})
+
+const openAddModal = () => {
+  addModal
+}
+
+const addNewTODO = () => {
+  openAddModal()
+}
+</script>
+
+<template>
+  Hello from TODOList
+  <ul>
+    <li v-for="item in TODOArray" :key="item.value">
+      {{ item }}
+    </li>
+  </ul>
+  <button @click="addNewTODO">Add new TODO</button>
+</template>
+
+<style scoped></style>
---
+++ src/components/addModal.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { reactive } from 'vue'
+
+const TODOtitle = ref('')
+const newTODOInfo = reactive({})
+
+const createNewTODO = () => {
+  newTODOInfo = { ...newTODOInfo, TODOtitle }
+}
+</script>
+
+<template>
+  Enter your information:
+  <input v-model="TODOtitle" />
+  <button @click="createNewTODO">Create</button>
+</template>
+
+<style scoped></style>
