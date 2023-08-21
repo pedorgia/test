e9884d45896e58f5338e9a6badc63427fb7f28fe
Yana <60299y@mail.ru>
2023-08-21 16:08:57 +0200

    cont

--- src/services/store.js
+++ src/services/store.js
@@ -5,13 +5,21 @@ export const store = reactive({
   addNewTODO(newTODO) {
     this.TODOArray.push(newTODO)
   },
-  deleteTODO(deleteIndex){
+  deleteTODO(deleteIndex) {
     this.TODOArray = this.TODOArray.filter((_, index) => index != deleteIndex)
   },
-  editTextInTask(TODOIndex, taskIndex, newText){
+  editTextInTask(TODOIndex, taskIndex, newText) {
     this.TODOArray[TODOIndex].tasks[taskIndex].text = newText
   },
-  editDoneStatusInTask(TODOIndex, taskIndex, newDoneStatus){
+  editDoneStatusInTask(TODOIndex, taskIndex, newDoneStatus) {
     this.TODOArray[TODOIndex].tasks[taskIndex].doneStatus = newDoneStatus
+  },
+  addNewTask(TODOIndex, newTask) {
+    this.TODOArray[TODOIndex].tasks.push(newTask)
+  },
+  deleteTask(TODOIndex, taskIndex) {
+    this.TODOArray[TODOIndex].tasks = this.TODOArray[TODOIndex].tasks.filter(
+      (_, ind) => ind != taskIndex
+    )
   }
 })
